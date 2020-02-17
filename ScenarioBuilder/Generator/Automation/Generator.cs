08c69339b70847072817dc928c5005a9bd27a69b
chrisfoster186 <chrisfoster186@googlemail.com>
2020-02-17 11:03:44 +0000

    Adds automation scenarios

---
+++ ScenarioBuilder/Generator/Automation/Generator.cs
@@ -0,0 +1,48 @@
+﻿using ScenarioBuilder.Builders;
+using ScenarioBuilder.Models;
+
+namespace ScenarioBuilder.Generator.Automation
+{
+    public class Generator
+    {
+        public static void Cohort_Provider_Draft(int size, bool nonLevyEmployer)
+        {
+            var builder = new CohortBuilder();
+
+            builder
+                .WithDefaultProvider()
+                .WithDefaultEmployer()
+                .WithEditStatus(EditStatus.Provider)
+                .WithLastAction(LastAction.None)
+                .WithCommitmentStatus(CommitmentStatus.New)
+                .WithApprenticeships(size);
+
+            if (nonLevyEmployer)
+            {
+                builder.WithNonLevyEmployer();
+            }
+
+            builder.Build();
+        }
+
+        public static void Cohort_Employer_Draft(int size, bool nonLevyEmployer)
+        {
+            var builder = new CohortBuilder();
+
+            builder
+                .WithDefaultProvider()
+                .WithDefaultEmployer()
+                .WithEditStatus(EditStatus.Employer)
+                .WithLastAction(LastAction.None)
+                .WithCommitmentStatus(CommitmentStatus.New)
+                .WithApprenticeships(size);
+
+            if (nonLevyEmployer)
+            {
+                builder.WithNonLevyEmployer();
+            }
+
+            builder.Build();
+        }
+    }
+}
